f53c7aebcf1a67b173450b2967034f15185bd0ca
Vagner Souza <66963813+vagnersouza23@users.noreply.github.com>
2024-06-05 06:23:12 -0300

    20240605

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,30 @@
+const elementoChute = document.getElementById("chute")
+
+
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+
+recognition.start()
+
+recognition.addEventListener('result', onSpeech)
+
+function onSpeech(e){
+
+    let chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+}
+
+function exibeChuteNaTela(chute){
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}<span>
+    
+    `
+
+}
+
+
+
+
